e48585822609b9ccfcd5fb67c4f6624220a67c12
bpds <bpds@me.com>
2020-06-30 15:05:30 +0200

    Try to install "Directly La-playlist.deb"

--- stage5/00-la-playlist/00-run.sh
+++ stage5/00-la-playlist/00-run.sh
@@ -1,6 +1,19 @@
 #!/bin/bash -e
 
-on_chroot << EOF
-  # ACTIVATION_KEY should be defined in the config.
-  (curl -s https://dev.the-playlist.com/assets/linux/install.sh) | bash -s - "${ACTIVATION_KEY}"
-EOF
+apt-get install files/La-playlist.deb
+
+EXE="la-playlist"
+
+if [[ -z "${ACTIVATION_KEY}" ]]; then
+  echo "Activation key is not defined"
+else
+  LOCAL_USER="pi"
+  CONF_FOLDER="${ROOTFS_DIR}/home/${LOCAL_USER}/.config/${PRODUCT_NAME}"
+  KEY_PATH="${CONF_FOLDER}/activation"
+  mkdir -p "${CONF_FOLDER}"
+  chown "${LOCAL_USER}:${LOCAL_USER}" "${CONF_FOLDER}"
+  touch "${KEY_PATH}"
+  echo "${ACTIVATION_KEY}" > "${KEY_PATH}"
+  chown "${LOCAL_USER}:${LOCAL_USER}" "${KEY_PATH}"
+  echo "The Activation Key has been copied to ${KEY_PATH}"
+fi
